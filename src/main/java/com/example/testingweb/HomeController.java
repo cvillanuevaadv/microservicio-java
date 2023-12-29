18db2a5e36fc95d093c9a726f08eecb62841d88e
Zander Villanueva <40078839+cvillanuevaadv@users.noreply.github.com>
2023-12-28 21:20:42 -0500

    Se agregan más vulnerabilidades

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -10,6 +10,11 @@ public class HomeController {
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
 		String password = "1234";
+		String password2 = "1234";
+		String password3 = "1234";
+		String password4 = "1234";
+		String password5 = "1234";
+		String password6 = "1234";
 		return "Hola, soy Carlos Villanueva";
 	}
 
