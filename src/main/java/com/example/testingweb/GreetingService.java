1831e3e286e28bf3ff14babef3df6fc125fcfb6a
Zander Villanueva <40078839+cvillanuevaadv@users.noreply.github.com>
2023-12-28 21:21:17 -0500

    Se agregan más vulnerabilidades

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
+		String password8 = "1234";
+		String password9 = "1234";
+		String password10 = "1234";
 		return "Hola, soy Carlos Villanueva";
 	}
 }
